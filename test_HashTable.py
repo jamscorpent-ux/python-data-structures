65d6b36e36af85820134cf4081f10b655c4cefd6
jamscorpent-ux <jams.corpent@gmail.com>
2026-05-26 11:42:20 +0800

    Create test_HashTable.py

---
+++ test_HashTable.py
@@ -0,0 +1,25 @@
+import unittest
+from hash_table import HashTable # Pastikan nama fail anda hash_table.py
+
+class TestHashTable(unittest.TestCase):
+    def setUp(self):
+        self.ht = HashTable()
+
+    def test_add_and_lookup(self):
+        self.ht.add('golf', 'sport')
+        self.assertEqual(self.ht.lookup('golf'), 'sport')
+
+    def test_collision(self):
+        # fcc dan cfc menghasilkan hash yang sama (300)
+        self.ht.add('fcc', 'coding')
+        self.ht.add('cfc', 'chemical')
+        self.assertEqual(self.ht.lookup('fcc'), 'coding')
+        self.assertEqual(self.ht.lookup('cfc'), 'chemical')
+
+    def test_remove(self):
+        self.ht.add('golf', 'sport')
+        self.ht.remove('golf')
+        self.assertIsNone(self.ht.lookup('golf'))
+
+if __name__ == '__main__':
+    unittest.main()
